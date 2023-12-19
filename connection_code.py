fc08cd20cea3123e11458ad24c84bbab75cab439
MicMaus <michael.yasynskyi@gmail.com>
2023-12-19 17:10:31 +0100

    1st pull

--- connection_code.py
+++ connection_code.py
@@ -1,18 +1,18 @@
 from mongoengine import connect
 
-# import configparser
+import configparser
 
-# config = configparser.ConfigParser()
-# config.read("./config.ini")
+config = configparser.ConfigParser()
+config.read("./config.ini")
 
-# mongo_user = config.get(section="DB", option="USER")
-# mongodb_pass = config.get(section="DB", option="PASS")
-# db_name = config.get(section="DB", option="DB_NAME")
-# domain = config.get(section="DB", option="DOMAIN")
+mongo_user = config.get(section="DB", option="USER")
+mongodb_pass = config.get(section="DB", option="PASS")
+db_name = config.get(section="DB", option="DB_NAME")
+domain = config.get(section="DB", option="DOMAIN")
 
-# url = f"mongodb+srv://{mongo_user}:{mongodb_pass}@{domain}/{db_name}?retryWrites=true&w=majority"
-url = "mongodb+srv://jasinsky:BmCJg7doGgfFEACx@mj.l7d1sxj.mongodb.net/?retryWrites=true&w=majority"
-db_name = "hw8"
+url = f"mongodb+srv://{mongo_user}:{mongodb_pass}@{domain}/{db_name}?retryWrites=true&w=majority"
+# url = "mongodb+srv://jasinsky:BmCJg7doGgfFEACx@mj.l7d1sxj.mongodb.net/?retryWrites=true&w=majority"
+# db_name = "hw8"
 
 try:
     connect(db=db_name, host=url)
